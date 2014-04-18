1a3c632e529f66ce3969ff873b5215a5784bec50
Swamp Ig <kerbal@ninjateaparty.com>
2014-04-19 07:36:24 +0800

    Moved code for dependency checking out
    Removed dependency checking from message service

---
+++ Source/PartDependencyChecker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KSPAPIExtensions
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, false)]
+    internal class PartDependencyChecker : MonoBehaviour
+    {
+
+        internal void Update()
+        {
+            // Do the requires thing. This needs to get moved to a different class.
+            HashSet<string> assem = new HashSet<string>();
+            foreach (var a in AssemblyLoader.loadedAssemblies)
+                assem.Add(a.assembly.GetName().Name);
+
+            foreach (UrlDir.UrlConfig urlConf in GameDatabase.Instance.root.AllConfigs)
+            {
+                if (urlConf.type != "PART")
+                    continue;
+
+                ConfigNode part = urlConf.config;
+                if (CheckPartRequiresAssembly(assem, part))
+                    continue;
+            }
+        }
+
+        private static bool CheckPartRequiresAssembly(HashSet<string> assem, ConfigNode part)
+        {
+            string partName = part.GetValue("name");
+
+            foreach (string keyValue in part.GetValues("RequiresAssembly"))
+            {
+                foreach (string split in keyValue.Split(','))
+                {
+                    string value = split.Trim();
+                    if (!string.IsNullOrEmpty(value) && (value[0] == '!' ? assem.Contains(value.Substring(1).Trim()) : !assem.Contains(value)))
+                    {
+                        Debug.Log("[PartMessageService] removing part " + partName + " due to dependency requirements not met");
+                        part.ClearData();
+                        return true;
+                    }
+                }
+            }
+
+            part.RemoveValues("RequiresAssembly");
+            return false;
+        }
+    }
+}
