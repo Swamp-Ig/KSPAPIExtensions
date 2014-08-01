d775479bde9c52fed5825798bbb41d4a1c87fd08
Bill Currie <bill@taniwha.org>
2014-08-01 23:51:59 +0900

    Make the GameObject extensions internal.
    So I don't have to document them.

--- Source/CommonAPI/GameObjectExt.cs
+++ Source/CommonAPI/GameObjectExt.cs
@@ -6,14 +6,14 @@ using UnityEngine;
 using KSP.IO;
 
 namespace KSPAPIExtensions {
-	public static class GameObjectExtension
+	internal static class GameObjectExtension
 	{
-		public static T AddTaggedComponent<T> (this GameObject go) where T : Component
+		internal static T AddTaggedComponent<T> (this GameObject go) where T : Component
 		{
 			Type taggedType = SystemUtils.VersionTaggedType(typeof(T));
 			return (T)go.AddComponent(taggedType);
 		}
-		public static T GetTaggedComponent<T> (this GameObject go) where T : Component
+		internal static T GetTaggedComponent<T> (this GameObject go) where T : Component
 		{
 			Type taggedType = SystemUtils.VersionTaggedType(typeof(T));
 			return (T)go.GetComponent(taggedType);
